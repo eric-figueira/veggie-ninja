b4942cbc8af1b08467ea9acc4c0110c8447f5938
eric-figueira <eric.figueira.07@gmail.com>
2023-11-21 09:41:08 -0300

    fix: update validation

--- entity.py
+++ entity.py
@@ -38,11 +38,13 @@ class Entity(pygame.sprite.Sprite):
         self.rect.x = self.rect.x + self.velocity_x
 
         if self.rect.bottom > SCREEN_HEIGHT + IMAGE_SIZE:
-            if self.rect.left + IMAGE_SIZE >= 0 and self.rect.right - IMAGE_SIZE < SCREEN_WIDTH:
-              if self.is_vegetable:
-                  self.game_lost_callback(True)
-              else:
-                  self.reset()
+            if self.is_vegetable:
+                if self.rect.left + IMAGE_SIZE >= 0 and self.rect.right - IMAGE_SIZE < SCREEN_WIDTH:
+                    self.game_lost_callback(True)
+                else:
+                    self.reset()
+            else:
+                self.reset()
 
     def get_velocity_x(self):
         if self.rect.x < SCREEN_WIDTH // 2:
